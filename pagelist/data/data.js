3aec6172d179908c28e07c5e57b54d2cd6e70282
Juyeon-0919 <60646121+Juyeon-0919@users.noreply.github.com>
2021-03-30 11:35:09 +0900

    init

---
+++ pagelist/data/data.js
@@ -0,0 +1,101 @@
+var data = {};
+data.page = {
+    'title': "메인",
+    'list': [
+        {
+            "stateClass": "complete",
+            "url": "ui_ma_01",
+            "part": "pages",
+            "page": "메인화면"
+        },
+        {
+            "stateClass": "complete",
+            "url": "ui_error",
+            "part": "pages",
+            "page": "화면 오류 페이지"
+        },
+        {
+            "stateClass": "complete",
+            "url": "화면오류(공동인증서)",
+            "part": "pages",
+            "page": "화면오류(공동인증서)"
+        },
+        {
+            "stateClass": "complete",
+            "url": "ui_m_copy",
+            "part": "pages",
+            "page": "모바일_공인인증서복사"
+        },
+        {
+            "stateClass": "complete",
+            "url": "ui_intro_mac_00",
+            "part": "pages",
+            "page": "PC_02_공인인증서복사_01_mac인경우"
+        },
+        {
+            "stateClass": "complete",
+            "url": "ui_intro_01",
+            "part": "pages",
+            "page": "PC_02_공인인증서복사_01_복사하기"
+        },
+        {
+            "stateClass": "complete",
+            "url": "ui_intro_03",
+            "part": "pages",
+            "page": "PC_02_공인인증서복사_02_내보내기"
+        },
+        {
+            "stateClass": "complete",
+            "url": "ui_intro_04",
+            "part": "pages",
+            "page": "PC_02_공인인증서복사_03_완료"
+        },
+    ]
+};
+data.terms = {
+    'title': "약관동의",
+    'list': [
+        {
+            "stateClass": "complete",
+            "url": "ui_terms_of_service_01",
+            "part": "pages",
+            "page": "약관동의 1"
+        },
+        {
+            "stateClass": "complete",
+            "url": "ui_terms_of_service_02",
+            "part": "pages",
+            "page": "약관동의 2"
+        },
+        {
+            "stateClass": "complete",
+            "url": "ui_terms_of_service_03",
+            "part": "pages",
+            "page": "약관동의 3"
+        },
+        {
+            "stateClass": "complete",
+            "url": "ui_terms_of_service_04",
+            "part": "pages",
+            "page": "약관동의 4"
+        },
+        {
+            "stateClass": "complete",
+            "url": "ui_terms_of_service_05",
+            "part": "pages",
+            "page": "약관동의 5"
+        },
+        {
+            "stateClass": "complete",
+            "url": "ui_terms_of_service_06",
+            "part": "pages",
+            "page": "약관동의 6"
+        },
+        {
+            "stateClass": "complete",
+            "url": "ui_terms_of_service_07",
+            "part": "pages",
+            "page": "약관동의 7"
+        },
+    ]
+};
